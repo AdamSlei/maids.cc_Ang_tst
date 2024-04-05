6b7779f98ba765cb7c03108846f0e679ab002c2c
AdamSlei <sleimanadam6@gmail.com>
2024-04-05 20:40:07 +0300

    fixed the cache service

--- angular-maids-test/src/app/services/data/data.service.ts
+++ angular-maids-test/src/app/services/data/data.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { User, UserResponse, UsersResponse } from '../../models/user.model';
+import { map, catchError, tap } from 'rxjs/operators';
+import { User } from '../../models/user.model';
 import { CacheService } from '../cache/cache.service';
 
 @Injectable({
@@ -11,31 +11,36 @@ import { CacheService } from '../cache/cache.service';
 export class DataService {
   constructor(private http: HttpClient, private cacheService: CacheService) {}
 
-  getUsers(page: number): Observable<any> {
+  getUsers(page: number): Observable<User[]> {
     const cacheKey = `users-page-${page}`;
     if (this.cacheService.has(cacheKey)) {
       return of(this.cacheService.get(cacheKey));
     }
 
-    return this.http.get(`https://reqres.in/api/users?page=${page}`).pipe(
-      map(response => {
-        this.cacheService.set(cacheKey, response);
-        return response;
-      })
+    return this.http.get<{ data: User[] }>(`https://reqres.in/api/users?page=${page}`).pipe(
+      map(response => response.data),
+      tap(data => this.cacheService.set(cacheKey, data)),
+      catchError(this.handleError<User[]>('getUsers', []))
     );
   }
 
-  getUser(id: number): Observable<any> {
+  getUser(id: number): Observable<User> {
     const cacheKey = `user-${id}`;
     if (this.cacheService.has(cacheKey)) {
       return of(this.cacheService.get(cacheKey));
     }
   
-    return this.http.get(`https://reqres.in/api/users/${id}`).pipe(
-      map(response => {
-        this.cacheService.set(cacheKey, response);
-        return response;
-      })
+    return this.http.get<{ data: User }>(`https://reqres.in/api/users/${id}`).pipe(
+      map(response => response.data),
+      tap(data => this.cacheService.set(cacheKey, data)),
+      catchError(this.handleError<User>('getUser'))
     );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
